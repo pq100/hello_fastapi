de28f4e1b01247e060327c780ea8f119d235c4ef
pq100 <system32bit@naver.com>
2024-08-12 17:35:55 +0900

    경로 매개변수

--- hello3_fastapi.py
+++ hello3_fastapi.py
@@ -1,20 +1,23 @@
 from fastapi import FastAPI
 
-
 # fastapi 앱 실행 방법
-# python -m uvicorn 파일명 : app --reload
-# python -m uvicorn hello_fastapi:app --reload
 
 app = FastAPI()
 
 @app.get('/')
 def index():
-    return 'Hello, World!!, again!!'
+    return 'Hello, World!!'
+
+@app.get('/sayHello')
+def sayhello(msg :str):
+    return f'Hello, {msg}!!'
+
+@app.get('/sayAgain/{msg}')
+def sayagain(msg :str):
+    return f'Hello, {msg}!!'
 
 # __name__: 실행중인 모듈 이름을 의미하는 매직키워드
 # 만일, 파일을 직접 실행하면 __name__의 이름은 __main__으로 자동지정
-
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run('hello2_fastapi:app', reload=True)
-
+    uvicorn.run('hello3_fastapi:app', reload=True)
